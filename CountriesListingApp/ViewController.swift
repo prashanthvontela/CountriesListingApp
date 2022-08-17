0518f9d113690e66d5642c34672736c409fa9eb7
Prashanth Vontela <prashanth.r.vontela@gmail.com>
2022-08-17 17:42:35 -0500

    Initial Commit

---
+++ CountriesListingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountriesListingApp
+//
+//  Created by lpalle on 8/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
